8d4c4066e591052affb0cba1854f959c36096fa3
Leo Volwahsen <volwahsenl@gmail.com>
2022-06-29 13:40:38 +0200

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+let vornamen = "Leo";
+let land = "Deutschland";
+console.log("Hallo ich bin " + vornamen + " und komme aus " + land + ".");
+let essen = "Knödel mit Sose";
+let essenZwei = "Schweinzhaxe";
+console.log("Mein Lieblingsessen sind " + essen + " und " + essenZwei + ".");
+let alter = 24;
+let geburtsJahr = 1997;
+console.log(
+    "Ich bin " + alter + " Jahre alt und " + geburtsJahr + " " + "geboren."
+);
